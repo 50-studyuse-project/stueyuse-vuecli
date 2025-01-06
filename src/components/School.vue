53028bab7292b119e05fbbc9cf4dc181298dcdf1
awesome <taycancille@outlook.com>
2025-01-06 20:59:45 +0800

    feat

--- src/components/School.vue
+++ src/components/School.vue
@@ -1,10 +1,12 @@
 <template>
     <div>
-        学校的名称是：{{name}}
+        这是学校组件，学校的名称是：{{name}}
     </div>
 </template>
 
 <script>
+    import pubsub from 'pubsub-js'; // 引入pubsub-js
+
     export default
     {
         name: 'School',
@@ -13,6 +15,10 @@
             return {
                 name: '北京小学校'
             };
+        },
+        mounted: function ()
+        {
+            pubsub.publish('sName', {a: 1, b: '这是b'})
         }
     }
 </script>
---
+++ src/components/Student.vue
@@ -0,0 +1,22 @@
+<template>
+    <div el="app">
+        这是Student组件，学生名是：{{stuName}}
+    </div>
+</template>
+
+<script>
+    export default
+    {
+        el: '#app',
+        name: "Student",
+        data: function ()
+        {
+            return {
+                stuName: '张三'
+            };
+        }
+    }
+</script>
+
+<style scoped>
+</style>
