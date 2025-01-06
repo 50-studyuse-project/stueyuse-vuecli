6b8858d1dcdbea58606670d4a51b1771ddd140fd
awesome <taycancille@outlook.com>
2025-01-06 20:22:20 +0800

    init

---
+++ src/components/School.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        学校的名称是：{{name}}
+    </div>
+</template>
+
+<script>
+    export default
+    {
+        name: 'School',
+        data: function ()
+        {
+            return {
+                name: '北京小学校'
+            };
+        }
+    }
+</script>
+
+<style scoped>
+</style>
